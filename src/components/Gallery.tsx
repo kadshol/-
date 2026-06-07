ecffcbd579c944ce8ba9868fa056bda5774dd4cb
kadshol <145913126+kadshol@users.noreply.github.com>
2026-06-07 17:45:25 +0300

    Fix image path in Gallery component

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import { motion } from 'framer-motion';
 
 const images = [
-  '/images/2.png', '/images/3.jpg', '/images/4.jpg', '/images/5.jpg',
+  'images/2.png', '/images/3.jpg', '/images/4.jpg', '/images/5.jpg',
   '/images/6.jpg', '/images/7.jpg', '/images/8.jpg', '/images/9.jpg',
   '/images/10.jpg', '/images/11.jpg', '/images/12.jpg', '/images/13.jpg',
 ];
